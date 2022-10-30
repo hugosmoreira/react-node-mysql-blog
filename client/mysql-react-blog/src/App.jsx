be435f664861964bdfb3fd6c43b58de7bb6aa2b0
hugosmoreira <hugo.n4@gmail.com>
2022-10-29 20:27:36 -0700

    update

--- client/mysql-react-blog/src/App.jsx
+++ client/mysql-react-blog/src/App.jsx
@@ -19,7 +19,7 @@ const Layout = () => {
   return (
     <>
       <Navbar />
-      
+      <Home />
       <Footer />
     </>
   );
--- client/mysql-react-blog/src/pages/Login.jsx
+++ client/mysql-react-blog/src/pages/Login.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
     <div className='auth'>
       <h1>Login</h1>
       <form>
-        <input type="text" placeholder='username' />
-        <input type="password" placeholder='password' />
-        <button className='btn'>Login</button>
+        <input required type="text" placeholder='username' />
+        <input required type="email" placeholder='email' />
+        <input required type="password" placeholder='password' />
+        <button required className='btn'>Login</button>
+        <span>Don't you have an account?</span> <Link to="/register">Register</Link>
       </form>
     </div>
   )
